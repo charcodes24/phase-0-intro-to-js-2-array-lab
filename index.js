0df73c674689e836fae9f864ab2cd9018314b0f2
Charlene Johnson <charcodes24@gmail.com>
2021-03-18 18:28:35 -0700

    Completed assignment.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+};
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+};
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+};
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+};
+
+function appendCat(name) {
+    let catsAppended = cats.concat(name);
+    return catsAppended;
+};
+
+function prependCat(name) {
+    let catsPrepended = [name, ...cats];
+    return catsPrepended;
+};
+
+function removeLastCat() {
+    let catsMinusOne = cats.slice(0,2);
+    return catsMinusOne;
+};
+
+function removeFirstCat() {
+    let catsWithoutFirstCat = cats.slice(1);
+    return catsWithoutFirstCat;
+};
